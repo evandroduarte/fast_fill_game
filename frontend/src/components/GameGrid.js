edfb8c929107c2078fb53fd14a9727d2c21a9b23
Evandro Duarte de Castro Filho <duarte.evandro@gmail.com>
2025-03-04 17:54:57 -0300

    chore: Remove build files

--- frontend/src/components/GameGrid.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export const GameGrid = ({ grid, onCellClick, playerColor, isGameActive }) => {
-    return (_jsx("div", { className: "game-grid", children: grid.map((row, rowIndex) => (_jsx("div", { className: "grid-row", children: row.map((cell, colIndex) => (_jsx("div", { className: `grid-cell ${cell !== 'empty' ? cell : ''}`, onClick: () => {
-                    if (isGameActive && cell === 'empty' && playerColor) {
-                        onCellClick(rowIndex, colIndex);
-                    }
-                } }, `${rowIndex}-${colIndex}`))) }, rowIndex))) }));
-};
